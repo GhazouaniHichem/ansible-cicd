b07db3621e43ec6ef6cd75fd2e0e10309a253b0b
GhazouaniHichem <ghazouanih68@gmail.com>
2022-09-12 11:28:59 +0100

    Change groovy script

--- script.groovy
+++ script.groovy
@@ -11,7 +11,7 @@ def buildImage() {
 
 def deployApp() {
     echo 'deploying the application....'
-    sh 'ssh -i ~/web-server-key-pair.pem -o "StrictHostKeyChecking no" ec2-user@13.38.38.61 -y "git clone https://github.com/GhazouaniHichem/devops-cicd.git && cd devops-cicd&& docker-compose up -d"'
+//    sh 'ssh -i ~/web-server-key-pair.pem -o "StrictHostKeyChecking no" ec2-user@13.38.38.61 -y "git clone https://github.com/GhazouaniHichem/devops-cicd.git && cd devops-cicd&& docker-compose up -d"'
 //    sh 'git clone https://github.com/GhazouaniHichem/devops-cicd.git'
 //    sh 'cd devops-cicd'
 //    sh 'docker-compose up -d'
